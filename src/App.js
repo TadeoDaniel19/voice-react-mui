65d953fe4b407c13dfd6968ad9774ac4e6f5ee48
TadeoDaniel19 <hector_lectric@hotmail.com>
2021-04-13 23:13:59 -0500

    comp: add modal component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,68 @@ import React from "react";
 import { Typography, Grid, IconButton } from '@material-ui/core';
 import MicIcon from '@material-ui/icons/Mic';
 import Footer from './components/footer/index';
+import SimpleModal from './components/modal/index';
 import SpeechRecognition, {
   useSpeechRecognition
 } from "react-speech-recognition";
+
 import './App.css';
 
 function App() {
+  const [open, setOpen] = React.useState(false);
   const commands = [
     {
-      command: "Busca múltiple",
-      callback: () => {
-        window.open("https://www.adidas.mx/");
-        window.open("https://www.miniso.com.mx/");
-        window.open("https://www.hibobi.com/");
-        window.open("https://vertiche.mx/");
-        window.open("https://www.cyamoda.com/");
+      command: "Busca múltiple *",
+      callback: (answer) => {
+        window.open(`https://www.adidas.mx/${answer}`);
+        window.open(`https://www.miniso.com.mx/${answer}`);
+        window.open(`https://www.hibobi.com/${answer}`);
+        window.open(`https://vertiche.mx/${answer}`);
+        window.open(`https://www.cyamoda.com/${answer}`);
+      },
+    },
+    {
+      command: "Busca adidas *",
+      callback: (answer) => {
+        window.open(`https://www.adidas.mx/${answer}`);
+
+      },
+    },
+    {
+      command: "Busca miniso *",
+      callback: (answer) => {
+        window.open(`https://www.miniso.com.mx/${answer}`);
+
+      },
+    },
+    {
+      command: "Busca hibobi *",
+      callback: (answer) => {
+        window.open(`https://www.hibobi.com/${answer}`);
+
+      },
+    },
+    {
+      command: "Busca vertiche *",
+      callback: (answer) => {
+        window.open(`https://vertiche.mx/${answer}`);
+
+      },
+    },
+    {
+      command: "Busca c a *",
+      callback: (answer) => {
+        window.open(`https://www.cyamoda.com/${answer}`);
+
       },
     },
     {
       command: "Ayuda del sitio",
       callback: () => {
-        alert(
-          '1.- Para mostar todas las páginas que tengo memorizadas di: *Busca multiple*, 2.- Para abrir una pagina en especifico di: *Abre*, 3.- Para cerrar la app di: *salir del sitio*'
-        )
+/*         alert(
+          '1.- Para mostar todas las páginas que tengo memorizadas di: *Busca multiple (seguido de la palabra clave)*, 2.- Para abrir una pagina en especifico di: *Abre*, 3.- Para cerrar la app di: *salir del sitio*'
+        ) */
+        handleOpen()
       },
     },
     {
@@ -43,10 +82,19 @@ function App() {
 
   const { transcript } = useSpeechRecognition({ commands });
   const handleCloseWindow = () => {
-    console.log('si entra aqui')
-    window.open("about:blank", "_self");
-    window.close();
+    if (window.confirm("¿Deseas salir?")) {
+      window.open("about:blank", "_self");
+      window.close();
+    }
+ 
   }
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null;
   }
@@ -67,7 +115,7 @@ function App() {
                 height: 60,
             }}/>
         </IconButton>
-
+        <SimpleModal handleClose={handleClose} handleOpen={handleOpen} open={open}/>
       </div>
       <Grid>
       <Footer />
